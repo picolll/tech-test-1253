9522d187037618c93368663b65eef2486cfef457
Piotr Lipski <piotr.lipski.it@gmail.com>
2025-09-21 18:39:01 +0100

    Adding AGGrid to display Excel styled table

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,55 @@
+import { AgGridReact } from 'ag-grid-react';
 import './App.css'
+import { ModuleRegistry, AllCommunityModule } from 'ag-grid-community';
+import { RowNumbersModule, CellSelectionModule } from 'ag-grid-enterprise';
+import { useState } from 'react';
+
+ModuleRegistry.registerModules([AllCommunityModule, RowNumbersModule, CellSelectionModule]);
+
+const cols = ["A", "B", "C"];
+const rows = [1, 2, 3];
+
+function makeEmptySheet() {
+  const sheet: Record<string, string> = {};
+  cols.forEach(
+    c => rows.forEach(
+      r => (
+        sheet[`${c}${r}`] = "test")
+    )
+  );
+  return sheet;
+}
 
 function App() {
+  const [sheet, setSheet] = useState<Record<string, string>>(makeEmptySheet());
+
+  type RowData = { id: number } & Record<string, string | number | undefined>;
+
+  const rowData: RowData[] = rows.map(r => {
+    const obj: RowData = { id: r };
+    cols.forEach(c => {
+      obj[c] = sheet[`${c}${r}`];
+    });
+    return obj;
+  });
+
+  const columnDefs = cols.map(c => ({
+    field: c,
+    width: 90,
+    cellDataType: false,
+  }));
 
   return (
     <>
-      <h1>Hello</h1>
+      <div className="wrapper">
+        <h1>Live Spreadsheet</h1>
+        <AgGridReact
+          rowNumbers
+          singleClickEdit
+          rowData={rowData}
+          columnDefs={columnDefs}
+        />
+      </div>
     </>
   )
 }
