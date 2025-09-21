2da75f5362b534fe404c5a01fd5d47f59e724bf4
Piotr Lipski <piotr.lipski.it@gmail.com>
2025-09-21 19:06:23 +0100

    Saving sheet value after editing.

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
 import { AgGridReact } from 'ag-grid-react';
 import './App.css'
-import { ModuleRegistry, AllCommunityModule } from 'ag-grid-community';
+import { ModuleRegistry, AllCommunityModule, type CellValueChangedEvent } from 'ag-grid-community';
 import { RowNumbersModule, CellSelectionModule } from 'ag-grid-enterprise';
 import { useState } from 'react';
 
 ModuleRegistry.registerModules([AllCommunityModule, RowNumbersModule, CellSelectionModule]);
 
 const cols = ["A", "B", "C"];
-const rows = [1, 2, 3];
+const rows = [1, 2, 3, 4, 5];
 
 function makeEmptySheet() {
   const sheet: Record<string, string> = {};
   cols.forEach(
     c => rows.forEach(
       r => (
-        sheet[`${c}${r}`] = "test")
+        sheet[`${c}${r}`] = "")
     )
   );
   return sheet;
@@ -22,6 +22,15 @@ function makeEmptySheet() {
 
 function App() {
   const [sheet, setSheet] = useState<Record<string, string>>(makeEmptySheet());
+
+  function onCellChange(params: CellValueChangedEvent) {
+    console.log('onCellChange', params);
+    setSheet(s => {
+      const key = `${params.colDef.field}${(params.rowIndex ?? 0) + 1}`;
+      const next = { ...s, [key]: params.newValue };
+      return next;
+    });
+  }
 
   type RowData = { id: number } & Record<string, string | number | undefined>;
 
@@ -36,6 +45,7 @@ function App() {
   const columnDefs = cols.map(c => ({
     field: c,
     width: 90,
+    editable: true,
     cellDataType: false,
   }));
 
@@ -48,6 +58,7 @@ function App() {
           singleClickEdit
           rowData={rowData}
           columnDefs={columnDefs}
+          onCellValueChanged={onCellChange}
         />
       </div>
     </>
